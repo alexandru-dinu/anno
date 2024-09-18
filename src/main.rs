d09f7b088d3ad37b41527668fe70c9c721f9867b
Alexandru Dinu <alex.dinu07@gmail.com>
2024-09-18 22:25:01 +0300

    Cleaner config parse & renaming.

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,48 @@
 use clap::{arg, command};
-use lazy_static::lazy_static;
-use serde_json::{Map, Value};
+use serde::Deserialize;
 use std::fs::File;
-use std::io::Read;
+use std::io::{self, Read};
 use std::process::{exit, Command};
 
 mod hashing;
 
 use hashing::hash_from_path;
 
+#[derive(Deserialize)]
 struct Config {
     anno_dir: String,
     editor: String,
 }
 
-lazy_static! {
-    static ref CONFIG: Config = {
-        let cfg = parse_json("config.json");
-        Config { // TODO: parse directly into struct?
-            anno_dir: cfg.get("anno_dir").unwrap().as_str().unwrap().to_string(),
-            editor: cfg.get("editor").unwrap().as_str().unwrap().to_string(),
-        }
-    };
+impl Config {
+    fn parse_from_file(path: &str) -> Result<Self, serde_json::Error> {
+        let contents = read_to_string(path).map_err(serde_json::Error::io)?;
+        serde_json::from_str(&contents)
+    }
 }
 
-fn read_to_string(path: &str) -> String {
-    let mut file = match File::open(path) {
-        Ok(file) => file,
-        Err(e) => {
-            eprintln!("{}: {}", e, path);
-            exit(1);
-        }
-    };
-
+fn read_to_string(path: &str) -> Result<String, io::Error> {
+    let mut file = File::open(path)?;
     let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
 
-    file.read_to_string(&mut contents)
-        .expect("Could not read file");
-
-    contents
+    Ok(contents)
 }
 
-fn parse_json(path: &str) -> Map<String, Value> {
-    // TODO: return dict
-    let contents = read_to_string(path);
-
-    let json: Value = serde_json::from_str(&contents).expect("Could not parse json");
-
-    json.as_object().unwrap().clone()
-}
-
-fn read_path(path: &str) {
+fn anno_read(config: &Config, path: &str) {
     let hash = hash_from_path(path);
-    let anno_path = format!("{}/{}", CONFIG.anno_dir, hash);
-
-    println!("{}", read_to_string(&anno_path));
+    let anno_path = format!("{}/{}", config.anno_dir, hash);
+    match read_to_string(&anno_path) {
+        Ok(contents) => println!("{}", contents),
+        Err(e) => eprintln!("Error reading file: {}", e),
+    }
 }
 
-fn write_path(path: &str) {
+fn anno_write(config: &Config, path: &str) {
     let hash = hash_from_path(path);
-    let anno_path = format!("{}/{}", CONFIG.anno_dir, hash);
+    let anno_path = format!("{}/{}", config.anno_dir, hash);
 
-    let status = Command::new(&CONFIG.editor)
+    let status = Command::new(&config.editor)
         .arg(anno_path)
         .status()
         .expect("Could not open editor");
@@ -84,20 +65,16 @@ fn main() {
                 .required_unless_present("read")
                 .conflicts_with("read"),
         )
-        .try_get_matches();
+        .try_get_matches()
+        .expect("Error parsing arguments");
 
-    if let Err(e) = matches {
-        println!("{}", e);
-        return;
-    }
-
-    let matches = matches.unwrap();
+    let config = Config::parse_from_file("config.json").expect("Failed to load config");
 
     if let Some(file) = matches.get_one::<String>("read") {
-        read_path(file);
+        anno_read(&config, file);
     }
 
     if let Some(file) = matches.get_one::<String>("write") {
-        write_path(file);
+        anno_write(&config, file);
     }
 }
