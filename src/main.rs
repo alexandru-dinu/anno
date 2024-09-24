d97dab4b01f6a5d523297804d2227f69b7b73e4f
Alexandru Dinu <alex.dinu07@gmail.com>
2024-09-24 20:40:24 +0300

    Read config from env. First working ver.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,34 @@
 use clap::{arg, command};
-use serde::Deserialize;
+use dirs::data_dir;
+use std::env;
+use std::fs;
 use std::fs::File;
 use std::io::{self, Read};
+use std::path::PathBuf;
 use std::process::{exit, Command};
 
 mod hashing;
 
 use hashing::hash_from_path;
 
-#[derive(Deserialize)]
 struct Config {
-    anno_dir: String,
+    anno_dir: PathBuf,
     editor: String,
 }
 
 impl Config {
-    fn parse_from_file(path: &str) -> Result<Self, serde_json::Error> {
-        let contents = read_to_string(path).map_err(serde_json::Error::io)?;
-        serde_json::from_str(&contents)
+    fn from_env() -> Config {
+        let default_anno_dir = data_dir().unwrap().join("anno");
+        let anno_dir = env::var("ANNO_DIR").map_or(default_anno_dir, PathBuf::from);
+        let editor = env::var("EDITOR").unwrap_or_else(|_| "nano".to_string());
+
+        fs::create_dir_all(&anno_dir).expect("Failed to create anno directory");
+
+        Config { anno_dir, editor }
     }
 }
 
-fn read_to_string(path: &str) -> Result<String, io::Error> {
+fn read_to_string(path: &PathBuf) -> Result<String, io::Error> {
     let mut file = File::open(path)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
@@ -31,16 +38,22 @@ fn read_to_string(path: &str) -> Result<String, io::Error> {
 
 fn anno_read(config: &Config, path: &str) {
     let hash = hash_from_path(path);
-    let anno_path = format!("{}/{}", config.anno_dir, hash);
+    let anno_path = config.anno_dir.join(hash).with_extension("anno");
+
     match read_to_string(&anno_path) {
-        Ok(contents) => println!("{}", contents),
-        Err(e) => eprintln!("Error reading file: {}", e),
+        Ok(contents) => {
+            println!("{}", contents);
+        }
+        Err(e) => {
+            eprintln!("Error reading anno file for `{}`: {}", path, e);
+            exit(1);
+        }
     }
 }
 
 fn anno_write(config: &Config, path: &str) {
     let hash = hash_from_path(path);
-    let anno_path = format!("{}/{}", config.anno_dir, hash);
+    let anno_path = config.anno_dir.join(hash).with_extension("anno");
 
     let status = Command::new(&config.editor)
         .arg(anno_path)
@@ -65,10 +78,9 @@ fn main() {
                 .required_unless_present("read")
                 .conflicts_with("read"),
         )
-        .try_get_matches()
-        .expect("Error parsing arguments");
+        .get_matches();
 
-    let config = Config::parse_from_file("config.json").expect("Failed to load config");
+    let config = Config::from_env();
 
     if let Some(file) = matches.get_one::<String>("read") {
         anno_read(&config, file);
